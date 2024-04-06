cd6eaee42db79e12554b886447432316dc84064c
Diego Pacheco <diego.pacheco.valdez@gmail.com>
2024-04-05 19:13:08 -0600

    implementation of CORS into the api

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app) 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
@@ -34,10 +36,10 @@ class Game(db.Model):
 
 @app.route('/')
 def home():
-    return 'Welcome to my game api'
+    return 'Welcome to my game API'
 
 @app.route(BASE_URL + 'add_game', methods=['POST'])
-def addGame():
+def add_game():
     if not request.json:
         abort(400, description='Missing body in request')
 
@@ -59,10 +61,10 @@ def addGame():
     return jsonify({'message': 'Game added successfully', 'game': game.show()}), 201
 
 @app.route(BASE_URL + 'change_game/<int:id>', methods=['PATCH'])
-def updateGame(id):
+def update_game(id):
     game = Game.query.get(id)
     if game is None:
-        abort(404, description="game not found!")
+        abort(404, description="Game not found!")
         
     if request.json == {}:
         abort(400, description='Missing required fields')
@@ -72,38 +74,38 @@ def updateGame(id):
         
     for field in required_fields:
         if field in request.json:
-            game.__dict__[field] = request.json[field]
+            setattr(game, field, request.json[field])
             changed = True
     
     db.session.commit()
-    if changed == True:
-        return jsonify('game has been changed has been succesfully changed'), 200
-    elif changed == False:
+    if changed:
+        return jsonify('Game has been changed successfully'), 200
+    else:
         return jsonify('There were no valid values to be changed', 400)
         
     
 @app.route(BASE_URL + 'show_games', methods=['GET'])
-def getgames():
-    allgames = Game.query.all()
-    return jsonify({'These are all your games': [game.show() for game in allgames]}), 200
+def get_games():
+    all_games = Game.query.all()
+    return jsonify([game.show() for game in all_games]), 200
 
 @app.route(BASE_URL + 'show_game/<int:id>', methods=['GET'])
-def getGame(id):
+def get_game(id):
     game = Game.query.get(id)
     if game is None:
-        abort(404, error="game not found!")
+        abort(404, description="Game not found!")
     return jsonify({'This is the game you asked for': game.show()}), 200
 
 @app.route(BASE_URL + 'delete_game/<int:id>', methods=['DELETE'])
-def deleteGame(id):
+def delete_game(id):
     game = Game.query.get(id)
     if game is None:
-        abort(404, error="game not found!")
+        abort(404, description="Game not found!")
     db.session.delete(game)
     db.session.commit()
     
-    return jsonify('game has been deleted'), 200
-    
+    return jsonify('Game has been deleted'), 200
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
